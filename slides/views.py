bc5b041827f03a5e05e5b2dfceeb2928759b5b0a
Miguel A Barbosa <miguelabarbosa@gmail.com>
2014-10-28 13:56:15 -0700

    adding some changes to search

--- slides/views.py
+++ slides/views.py
@@ -1,12 +1,10 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-import haystack_static_pages
-from haystack_static_pages.models import StaticPage
 import re
 from slides.forms import EmailUserCreationForm, SearchForm
 from haystack.query import SearchQuerySet
-from slides.models import Resource
+from slides.models import Resource, Person
 from bs4 import BeautifulSoup
 
 
@@ -38,21 +36,25 @@ def search_page(request):
         if form.is_valid():
             search_text = form.cleaned_data['search_text']  # strip out value from search form
             search_results = SearchQuerySet().filter(content=search_text)  # process woosh query using search+text
-            slides_results = []
-            resource_results = []
-            for result in search_results:
-
-                if re.search(r"haystack_static_pages.staticpage", result.id):
+            slides_results = []     # empty list for slide results
+            resource_results = []   # empty list for resource results
+            for result in search_results:   # loop over results
+                if re.search(r"haystack_static_pages.staticpage", result.id):  # if its a static page
                     soup = BeautifulSoup(result.content)
+                    slide_title = soup.find('h2').get_text().strip()  # grab slide title
+                    slide_content = soup.find_all(text=re.compile(search_text))
+                    slides_results.append({'result': result, 'title': slide_title, 'content': slide_content})
 
-                    slide_title = soup.find('h2').get_text().strip()
-                    slides_results.append([result, slide_title])
+                elif re.search(r"slides.resource", result.id):  # if its a resource
+                    #print result.creator.first_name
+                    #first_name = re.search(r" ", result.creator)  # grab first name
+                    #print first_name
+                    #last_name = re.search(r" ", result.creator)  # grab last name
+                    #print last_name
+                    #person = Person.objects.filter(first_name__Miguel) # find all the people
+                    resource_results.append({'result': result})  # append resource result to results list
 
-                elif re.search(r"slides.resource", result.id):
-                    resource_results.append(result)
-            print slides_results
             data = {'slides_results': slides_results, 'resource_results': resource_results}
-
             return render(request, "search_results.html",  data)
 
     else:
