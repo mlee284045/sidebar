02c108f12fa8cc0dbea4e3fa09c5df9f2d916f55
PatrickSeet <playlife00@yahoo.com>
2014-10-29 15:35:26 -0700

    updated title

--- slides/static/js/resources.js
+++ slides/static/js/resources.js
@@ -1,13 +1,6 @@
 
 $(document).ready(function() {
 
-    function getSlideTitle(){
-
-        var element = $('.present').find('h2');
-
-        return element[0].firstChild.nodeValue;
-    }
-
     function getForm() {
 
         var slideTitle;
@@ -22,7 +15,9 @@ $(document).ready(function() {
                     $('#displayResource').show();
                     slideTitle = getSlideTitle();
                     saveForm(slideTitle);
+//                    $('#displayResource').load();
                     location.reload();
+
                 });
             },
             error: function (e) {
@@ -49,24 +44,35 @@ $(document).ready(function() {
             }
         });
     }
+
+    //Add Resource form hide as default
+    $('#form_holder').hide();
+
+    //Clicking the save button hides Add Resource form,
+    //then shows accordion
+    $('#add_resource').on('click', function(){
+        getForm();
+        $('#displayResource').hide();
+        $('#form_holder').show();
+    });
+
     //clicking cancel hides Add Resource
     //and shows accordion
     $('#cancel').on('click',function () {
         $(this).parent().hide();
-//        $(this).fadeOut('fast');
 //        location.reload();
-//        $('#displayResource').show();
+        $('#displayResource').show();
+//        $('#displayResource').load();
         location.reload();
-        $('#displayResource').fadeIn('fast');
-
 //        $('#displayResource').load(document.URL);
 
     });
-    $("#add_resource").on('click', function () {
-        getForm();
-    });
 
-    $.ajax({
+});
+
+
+function loadAccordion(){
+        $.ajax({
         url: '/sidebar/',
         type: 'GET',
         success: function (data) {
@@ -78,39 +84,36 @@ $(document).ready(function() {
                 if (window.location == data[i].slide) {
                     currentSlide = i;
                 }
+//                $("#accordion").append('<p><a href="' + data[i].slide + '">' + data[i].title + '</a></p><div>' + data[i].creator + '<br>' + data[i].text + '<br>' + data[i].date + '</div>');
                 $("#accordion").append('<p><a href="' + data[i].slide + '">' + data[i].title + '</a></p><div>' + data[i].creator + '<br>' + data[i].text + '<br>' + data[i].date + '</div>');
+
             }
 
             //set <p> tag in accordion as link
-            $("#accordion").accordion({header: 'p'});
+//            $("#accordion").accordion({header: 'p'},{ active: currentSlide }, { heightStyle: "content" });
+            $("#accordion").accordion({header: 'p'},{ active: currentSlide }, { heightStyle: "content" });
+
             $("#accordion p a").click(function () {
                 window.location = $(this).attr('href');
                 return false;
             });
-            //initialize the accordion
-            $("#accordion").accordion({ active: currentSlide }, { heightStyle: "content" });
 
             // Hover states on the static widgets
-            $("#dialog-link, #icons li").hover(
-                function () {
-                    $(this).addClass("ui-state-hover");
-                },
-                function () {
-                    $(this).removeClass("ui-state-hover");
-                }
-            );
+//            $("#dialog-link, #icons li").hover(
+//                function () {
+//                    $(this).addClass("ui-state-hover");
+//                },
+//                function () {
+//                    $(this).removeClass("ui-state-hover");
+//                }
+//            );
         }
     });
+}
 
-    //Add Resource form hide as default
-    $('#form_holder').hide();
+function getSlideTitle(){
 
-    //Clicking the save button hides Add Resource form,
-    //then shows accordion
-    $('#add_resource').on('click', function(){
-        $('#displayResource').hide();
-        $('#form_holder').show();
-    });
-});
+    var element = $('.present').find('h2');
 
-
+    return element[0].firstChild.nodeValue;
+}
